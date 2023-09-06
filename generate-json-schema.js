f2ddb0bdb5f6eadc7451d79acf918f2cf94e3e52
Haakon Borch <haakon.borch@netlife.com>
2023-09-06 15:05:43 +0200

    :bug: fix generate-schema and add to gitignore

---
+++ generate-json-schema.js
@@ -0,0 +1,24 @@
+// main.js
+
+const tsj = require('ts-json-schema-generator');
+const fs = require('fs');
+
+/** @type {import('ts-json-schema-generator/dist/src/Config').Config} */
+const config = {
+  path: 'types.ts',
+  tsconfig: 'tsconfig.json',
+  type: 'Query', // Or <type-name> if you want to generate schema for that one type only
+};
+
+const output_path = 'json-schema/';
+const output_file = 'QuerySchema.json';
+
+const schema = tsj.createGenerator(config).createSchema(config.type);
+const schemaString = JSON.stringify(schema, null, 2);
+// create the folder if it doesnt exist
+if (!fs.existsSync(output_path)) {
+  fs.mkdirSync(output_path);
+}
+fs.writeFile(output_path + output_file, schemaString, (err) => {
+  if (err) throw err;
+});
